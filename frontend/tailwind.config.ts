e5db3d0b9269ff27c0e90fd93568fd758972179f
Shubham Mandal <shubham.m9022@gmail.com>
2025-06-07 18:50:34 +0530

    Updated code

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,16 @@
+
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        urbanist: ['Urbanist', 'sans-serif'],
+      },
+      colors: {
+        primary: '#06723e',
+        secondary: '#98bc5e',
+        'primary-dull': '#a5d6a7',
+        bg: '#e3f5e4',
+      }
+    }
+  }
+};
