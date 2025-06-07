39ffa0c96885ebb1a1fa64cd2eec4fc1ac5b8cc4
Shubham Mandal <shubham.m9022@gmail.com>
2025-06-07 12:04:54 +0530

    Updated some code

--- frontend/src/components/Bestseller.tsx
+++ frontend/src/components/Bestseller.tsx
@@ -29,7 +29,7 @@ const Bestseller = () => {
                                 <svg width="14" height="14" className='' viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" className='stroke-gray-900' stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
-                                Add
+                               <span className='hidden md:inline'>Add</span>
                             </button>
                         ) : (
                             <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-indigo-500/25 rounded select-none">
