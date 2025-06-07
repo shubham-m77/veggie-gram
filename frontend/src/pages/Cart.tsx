3fb347bcd58864820b709bac20f3311ad5fd91df
Shubham Mandal <shubham.m9022@gmail.com>
2025-06-07 12:10:49 +0530

    updated

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -187,11 +187,11 @@ if (cartArray.length === 0) {
                           </p>
                           <div className="text-primary ">
                               {!cartItems[product._id]? (
-                                  <button className="cursor-pointer bg-bg flex items-center transition ease-in hover:scale-95 justify-center gap-1 hover:bg-primary-dull border border-secondary md:w-[80px] w-[64px] h-[34px] rounded text-gray-900 font-medium" onClick={() => addToCart(product._id)} >
+                                  <button className="cursor-pointer bg-bg flex items-center transition ease-in hover:scale-95 justify-center gap-1 hover:bg-primary-dull border border-secondary md:w-[80px] w-[40px] h-[30px] md:h-[34px] rounded text-gray-900 font-medium" onClick={() => addToCart(product._id)} >
                                       <svg width="14" height="14" className='' viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                           <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" className='stroke-gray-900' stroke-linecap="round" stroke-linejoin="round" />
                                       </svg>
-                                      
+                                      <span className="hidden md:inline">Add</span>
                                   </button>
                               ) : (
                                   <div className="flex items-center justify-center gap-2 md:w-20 w-16 h-[34px] bg-indigo-500/25 rounded select-none">
